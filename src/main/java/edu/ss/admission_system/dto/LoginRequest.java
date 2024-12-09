0975e4934ae571a976599adaf32b90906198aed2
Ramesh M <137721657+Ramesh2815-art@users.noreply.github.com>
2024-12-09 20:22:44 +0530

    Add files via upload

---
+++ src/main/java/edu/ss/admission_system/dto/LoginRequest.java
@@ -0,0 +1,33 @@
+package edu.ss.admission_system.dto;
+ 
+
+
+
+
+public class LoginRequest {
+    private String email;
+    private String password;
+    private String PhoneNumber;
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getPhoneNumber() {
+		return PhoneNumber;
+	}
+	public void setPhoneNumber(String phoneNumber) {
+		PhoneNumber = phoneNumber;
+	}
+
+    // Getters and Setters
+    
+    
+}
